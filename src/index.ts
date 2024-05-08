f0aae3738539f0d4203763c8006180102e084e18
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-08 18:03:15 +0900

    標準入力後に1を出力するロジックを実装

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import * as console from "node:console";
+
 export const main = (input: string) => 1;
+
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+const reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  console.log(main(line));
+});
