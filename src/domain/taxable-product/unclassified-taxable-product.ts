153e05a0a57435da64d50e6b027ed0b6bae95c5c
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-11 23:16:14 +0900

    unclassified-taxable-product.tsを作成する

---
+++ src/domain/taxable-product/unclassified-taxable-product.ts
@@ -0,0 +1,50 @@
+import {OrderedProducts, Product} from "../product/product";
+
+export type UnClassifiedIntegratedAsset = {
+  type: "UnClassifiedIntegratedAsset",
+  oralProduct: Product,
+  nonOralProduct: Product,
+}
+export type UnClassifiedSingleProduct = {
+  type: "UnClassifiedSingleProduct",
+  product: Product,
+}
+export type UnClassifiedProduct = UnClassifiedIntegratedAsset | UnClassifiedSingleProduct
+
+
+// 注文内容から税率未分類の商品を作成する関数
+export function translateToUnclassifiedTaxRateProduct(orderedProducts: OrderedProducts): UnClassifiedProduct[] {
+  let unClassifiedProducts: UnClassifiedProduct[] = [];
+  let currentUnClassifiedProduct: Product | null = null;
+
+  for (const product of orderedProducts.products) {
+    if (currentUnClassifiedProduct === null) {
+      currentUnClassifiedProduct = product;
+    }
+    else if (currentUnClassifiedProduct.isOralProduct.value !== product.isOralProduct.value) {
+      const unClassifiedIntegratedAsset: UnClassifiedIntegratedAsset = {
+        type: "UnClassifiedIntegratedAsset",
+        oralProduct: currentUnClassifiedProduct.isOralProduct.value ? currentUnClassifiedProduct : product,
+        nonOralProduct: currentUnClassifiedProduct.isOralProduct.value ? product : currentUnClassifiedProduct,
+      };
+      unClassifiedProducts.push(unClassifiedIntegratedAsset);
+      currentUnClassifiedProduct = null;
+    }
+    else {
+      const unClassifiedSingleProduct: UnClassifiedSingleProduct = {
+        type: "UnClassifiedSingleProduct",
+        product: currentUnClassifiedProduct
+      };
+      unClassifiedProducts.push(unClassifiedSingleProduct)
+      currentUnClassifiedProduct = product;
+    }
+  }
+  if (currentUnClassifiedProduct !== null) {
+    unClassifiedProducts.push({
+      type: "UnClassifiedSingleProduct",
+      product: currentUnClassifiedProduct
+    });
+  }
+
+  return unClassifiedProducts;
+}
