0a6f422a7150b6ec16b5eb9fc5dbf5fe8f5e2367
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-15 00:08:39 +0900

    mainメソッドを修正する

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import * as console from "node:console";
+import {translateToWorkflowInput} from "./presentation/lib/translate-input-string";
+import {DeriveSumPriceWorkflow} from "./workflow/derive-sum-price-workflow";
 
-export const main = (input: string) => 1;
-
-
+export const main = (inputString: string) => {
+  const input = translateToWorkflowInput(inputString);
+  return new DeriveSumPriceWorkflow().deriveSumPrice(input);
+};
 
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
@@ -11,6 +14,7 @@ const reader = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
 reader.on('line', (line) => {
   console.log(main(line));
 });
