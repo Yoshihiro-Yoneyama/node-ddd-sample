65e9f7eecbfc483608d608fca86df8de65401fee
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-15 00:07:54 +0900

    derive-sum-price-workflow.tsを修正する

--- src/workflow/derive-sum-price-workflow.ts
+++ src/workflow/derive-sum-price-workflow.ts
@@ -1,19 +1,10 @@
 import {WorkflowInputs} from "./derive-sum-price-workflow-input";
-import {
-  createOrderedProduct,
-  createProduct,
-  DeliveryMethod,
-  DeliveryTo,
-  IsOralProduct,
-  ProductPrice,
-  ProductType,
-  ServiceType
-} from "../domain/ordered-product/ordered-product";
+import {createProduct, IsOralProduct, OrderedProducts, ProductPrice} from "../domain/ordered-product/ordered-product";
 import {translateToUnclassifiedTaxRateProduct} from "../domain/taxable-product/unclassified-taxable-product";
 import {
-  classifyToTaxableProduct,
   createTaxableProductAndTaxRate,
-  TaxableProductAndTaxRate
+  TaxableProductAndTaxRate,
+  translateToTaxableProduct
 } from "../domain/taxable-product/classified-taxable-product";
 import {checkIsEligibleForDiscount, discount} from "../domain/taxable-product/discount-rule";
 import {calculateTotalWithTax} from "../domain/taxable-product/calculate-total-with-tax";
@@ -21,27 +12,26 @@ import {calculateTotalWithTax} from "../domain/taxable-product/calculate-total-w
 export class DeriveSumPriceWorkflow {
   deriveSumPrice(inputs: WorkflowInputs) {
     // inputからOrderを作成する
-    const orderedProducts = createOrderedProduct(
-      inputs.map(input => createProduct(
-        input.productType as ProductType,
+    const orderedProducts: OrderedProducts = inputs
+      .map(input => createProduct(
+        input.productType,
         IsOralProduct(input.isOralProduct),
-        input.serviceType as ServiceType,
-        input.deliveryMethod as DeliveryMethod,
-        input.deliveryTo as DeliveryTo,
+        input.serviceType,
+        input.deliveryMethod,
+        input.deliveryTo,
         ProductPrice(input.price)
-      ))
-    )
+      ));
     // Orderから税率未分類の商品リストを作成する
     const unClassifiedProducts = translateToUnclassifiedTaxRateProduct(orderedProducts);
     // 税率未分類の商品からTaxableProductのリストを作成する
-    const classifiedProducts = classifyToTaxableProduct(unClassifiedProducts);
+    const classifiedProducts = translateToTaxableProduct(unClassifiedProducts);
     // TaxableProductのリストからTaxableProductAndTaxRateのリストを作成する
     const taxableProductAndTaxRateList: TaxableProductAndTaxRate[] = classifiedProducts
       .map(classifiedProduct => createTaxableProductAndTaxRate(classifiedProduct));
     // TaxableProductAndTaxRateのリストから合計金額を算出する
     const totalWithTax = calculateTotalWithTax(taxableProductAndTaxRateList);
     // 値引き対象か判定する
-    const isEligibleForDiscount = checkIsEligibleForDiscount(classifiedProducts);
+    const isEligibleForDiscount = checkIsEligibleForDiscount(orderedProducts);
     //　値引きルールを適用する
     const calculateInclusiveTotal = discount(totalWithTax, isEligibleForDiscount);
     //　最終金額を返す
