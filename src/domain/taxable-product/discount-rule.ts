50cbb3ad18bbabc97081cd39838763c6718af30e
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-15 15:30:31 +0900

    discount-rule.ts修正

--- src/domain/taxable-product/discount-rule.ts
+++ src/domain/taxable-product/discount-rule.ts
@@ -1,31 +1,17 @@
-import {Brand} from "../../lib/brand";
 import {OrderedProducts, ProductType} from "../ordered-product/ordered-product";
 import {isFoodAndBeverage} from "./unclassified-taxable-product";
 
 const DISCOUNT_RATE = 0.9;
 
-export type IsEligibleForDiscount = Brand<boolean, "IsEligibleForDiscount">;
-export function IsEligibleForDiscount(value: boolean): IsEligibleForDiscount {
-  return value as IsEligibleForDiscount;
-}
-
-export function checkIsEligibleForDiscount(orderedProducts: OrderedProducts): IsEligibleForDiscount {
-  let foodAndBeverageAndNewspaperPriceTotal = 0;
-  let otherPriceTotal = 0;
-  orderedProducts.forEach(orderedProduct => {
-    const i = isFoodAndBeverage(orderedProduct);
-    if (i || orderedProduct.productType === ProductType.Newspaper) {
-      foodAndBeverageAndNewspaperPriceTotal += orderedProduct.price;
-    } else {
-      otherPriceTotal += orderedProduct.price;
-    }
-  });
-  return IsEligibleForDiscount(foodAndBeverageAndNewspaperPriceTotal * 1.5 === otherPriceTotal);
-}
-
-export function discount(totalWithTax: number, isEligibleForDiscount: IsEligibleForDiscount): number {
-  if (isEligibleForDiscount) {
-    return Math.floor(totalWithTax * DISCOUNT_RATE);
-  }
-  return totalWithTax;
+export function applyDiscountRule(orderedProducts: OrderedProducts): (totalWithTax: number) => number {
+  const foodAndBeverageAndNewsPaperTotal = orderedProducts
+    .filter(orderedProducts => isFoodAndBeverage(orderedProducts) || orderedProducts.productType === ProductType.Newspaper)
+    .reduce((acc, crr) => acc + crr.price, 0);
+  const otherTotal = orderedProducts
+    .filter(orderedProducts => !isFoodAndBeverage(orderedProducts) && orderedProducts.productType !== ProductType.Newspaper)
+    .reduce((acc, crr) => acc + crr.price, 0);
+  const isEligibleForDiscount = foodAndBeverageAndNewsPaperTotal * 1.5 === otherTotal;
+  return isEligibleForDiscount
+    ? (totalWithTax: number) => Math.floor(totalWithTax * DISCOUNT_RATE)
+    : (totalWithTax: number) => totalWithTax;
 }
