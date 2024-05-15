25c0719b9aa4a4ab9a0949700a89eb48f2e1f50d
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-15 18:29:36 +0900

    input修正

--- src/workflow/derive-sum-price-workflow-input.ts
+++ src/workflow/derive-sum-price-workflow-input.ts
@@ -1,8 +1,10 @@
-export type WorkflowInputs = {
+export type WorkflowInput = {
   productType: "Book" | "Beverage" | "Alcohol" | "QuasiDrug" | "Newspaper" | "Medicine" | "Other" | "Food",
   isOralProduct: boolean,
   serviceType: "TakeOut" | "EatIn",
   deliveryMethod: "Catering" | "Delivery" | "InternetDelivery",
   deliveryTo: "House" | "NursingHome" | "Apartment" | "NoPlace",
   price: number,
-}[]
+}
+
+export type WorkflowInputs = WorkflowInput[];
