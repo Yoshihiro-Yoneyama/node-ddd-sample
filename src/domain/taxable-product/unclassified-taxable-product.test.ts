6691a0f6244b418e39ca6846cbdf03ed7946d94e
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-16 11:19:24 +0900

    unclassified-taxable-product.test.tsを修正する

--- src/domain/taxable-product/unclassified-taxable-product.test.ts
+++ src/domain/taxable-product/unclassified-taxable-product.test.ts
@@ -2,9 +2,9 @@ import {describe, expect, it} from "@jest/globals";
 import {
   createUnclassifiedIntegratedAsset,
   createUnclassifiedIntegratedAssetBundle,
-  extractUnclassifiedSingleProduct,
-  isFoodAndBeverage,
-  OralProductPrice
+  extractUnclassifiedSingleProduct, IsFoodAndBeverage,
+  isFoodAndBeverage, NonOralProductPrice,
+  OralProductPrice, SingleProductPrice, translateToUnclassifiedProduct
 } from "./unclassified-taxable-product";
 import {
   DeliveryMethod,
@@ -15,7 +15,6 @@ import {
   ProductType,
   ServiceType
 } from "../ordered-product/ordered-product";
-
 
 describe("unclassified Taxable Product", () => {
   describe("OralProductPrice", () => {
@@ -32,7 +31,63 @@ describe("unclassified Taxable Product", () => {
   });
 
   describe("translateToUnclassifiedProduct", () => {
+    it("未分類の一体資産の組から未分類の一体資産を作成する", () => {
+      const orderedProducts = [
+        {
+          id: OrderedProductId("test1"),
+          productType: ProductType.Newspaper,
+          isOralProduct: IsOralProduct(true),
+          serviceType: ServiceType.TakeOut,
+          deliveryMethod: DeliveryMethod.Catering,
+          deliveryTo: DeliveryTo.Apartment,
+          price: ProductPrice(1000),
+        },
+        {
+          id: OrderedProductId("test2"),
+          productType: ProductType.Book,
+          isOralProduct: IsOralProduct(true),
+          serviceType: ServiceType.TakeOut,
+          deliveryMethod: DeliveryMethod.Catering,
+          deliveryTo: DeliveryTo.Apartment,
+          price: ProductPrice(2000),
+        },
+        {
+          id: OrderedProductId("test3"),
+          productType: ProductType.QuasiDrug,
+          isOralProduct: IsOralProduct(false),
+          serviceType: ServiceType.TakeOut,
+          deliveryMethod: DeliveryMethod.Delivery,
+          deliveryTo: DeliveryTo.Apartment,
+          price: ProductPrice(3000),
+        }
+      ];
+      const actual = translateToUnclassifiedProduct(orderedProducts);
+      expect(actual).toEqual([
+        {
+          type: "UnclassifiedIntegratedAsset",
+          oralProduct: {
+            price: OralProductPrice(2000),
+            isFoodAndBeverage: IsFoodAndBeverage(false),
+          },
+          nonOralProduct: {
+            price: NonOralProductPrice(3000),
+          }
+        },
+        {
+          type: "UnclassifiedSingleProduct",
+          productType: ProductType.Newspaper,
+          singleProductPrice: SingleProductPrice(1000),
+          isOralProduct: IsOralProduct(true),
+          isFoodAndBeverage: IsFoodAndBeverage(false)
+        }
 
+      ]);
+    });
+    it("未分類の一体資産の組がない場合空の配列を返す", () => {
+      const pair = [];
+      const actual = translateToUnclassifiedProduct(pair);
+      expect(actual).toEqual([]);
+    });
   });
 
   describe("createUnclassifiedIntegratedAssetBundle", () => {
