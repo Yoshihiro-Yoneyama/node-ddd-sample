a1d426a6a46c0566a674fbdb684e69f05af1ef81
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-17 22:53:23 +0900

    ドキュメント作成

--- src/domain/taxable-product/discount-rule.ts
+++ src/domain/taxable-product/discount-rule.ts
@@ -6,7 +6,7 @@ import {isFoodAndBeverage} from "./unclassified-taxable-product";
  *
  * @remarks
  * 割引ルールの適用条件は飲食料品と新聞の税抜合計金額と \
- * その他の商品の税抜合計金額の比がちょうど2：3である \
+ * その他の商品の税抜合計金額の比がちょうど2：3の場合である \
  * この条件を満たす場合、最終的な税込合計金額から10%の値引きを行う関数を返す。 \
  *
  * @param orderedProducts
