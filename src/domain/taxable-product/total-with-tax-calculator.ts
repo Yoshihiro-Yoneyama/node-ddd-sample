84e24697ebf5e5755db0d56b4c8a10e5edcfb20d
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-17 15:33:31 +0900

    ドキュメント作成

--- src/domain/taxable-product/total-with-tax-calculator.ts
+++ src/domain/taxable-product/total-with-tax-calculator.ts
@@ -1,5 +1,11 @@
 import {TaxableProductAndTaxRate} from "./classified-taxable-product";
 
+/**
+ * 税率別の商品とリストから税込合計金額を計算する関数
+ *
+ * @param taxableProductAndTaxRateList 税率別の商品と税率の組のリスト
+ * @returns 税込合計金額
+ */
 export function calculateTotalWithTax(taxableProductAndTaxRateList: TaxableProductAndTaxRate[]): number {
   return taxableProductAndTaxRateList
     .reduce((totalWithTax, [taxableProduct, taxRate]) => {
